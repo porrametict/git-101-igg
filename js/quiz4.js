0365b42af3a4930f8bc5bb575eccf7c316927ac6
porrametict <60021414@up.ac.th>
2020-09-16 15:51:48 +0700

    quiz4

---
+++ js/quiz4.js
@@ -0,0 +1,5 @@
+let str1  = "Hello my name is Joke"
+let str2  = "Well open up your mind ans see like me"
+console.log(str1.length);
+console.log(str1.indexOf("Joke"));
+console.log(str2.slice(4,9))
