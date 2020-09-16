6b5b861b5123b8d69a9f57e10a94d17af4223932
porrametict <60021414@up.ac.th>
2020-09-16 13:53:48 +0700

    tetw

---
+++ js/quiz1.js
@@ -0,0 +1,9 @@
+let obj_me = {
+    first_name : "Porramet",
+    last_name : "Taengorn",
+    age : 21,
+    age : "Peet",
+    hobby : "play games",
+    phone_number : "09303225456",
+}
+console.log(obj_me);
