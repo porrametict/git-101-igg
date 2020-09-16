9e85ccede6254f8632f699a30b17dfc55a7d3f86
porrametict <60021414@up.ac.th>
2020-09-16 14:38:59 +0700

    Edit loop file
    loop description

--- js/loop.js
+++ js/loop.js
@@ -1,27 +1,35 @@
-const arr = [
-    "John",
-    "Jane",
-    "James",
-    "Joe",
-    "June",
-]
+// const arr = [
+//     "John",
+//     "Jane",
+//     "James",
+//     "Joe",
+//     "June",
+// ]
 
-for(let i=0;i < arr.length;i++){
-    console.log(arr[i]);
-}
+// for(let i=0;i < arr.length;i++){
+//     console.log(arr[i]);
+// }
 
-for(let index in arr) {
-    console.log(arr[index]);
-}
+// for(let index in arr) {
+//     console.log(arr[index]);
+// }
 
-arr.forEach((item,index)=>{
-    console.log(item,index);
+// arr.forEach((item,index)=>{
+//     console.log(item,index);
+// })
+
+// arr.map((item,index)=>{
+//     console.log(item,index);
+// // })
+
+// let val = arr.map((item,index)=>{
+//     return `${index}.${item}`
+// })
+
+// console.log(val);
+
+
+let fillterMoreThanZero = [-1,2,3,-6645,0,51,3].filter(e => {
+    return e > 0
 })
-
-arr.map((item,index)=>{
-    console.log(item,index);
-})
-
-arr.map((item,index)=>{
-    console.log(item,index);
-})
+console.log(fillterMoreThanZero);
