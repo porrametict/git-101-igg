1a933a1a5a06667c56c43b239277031bf3c3d932
porrametict <60021414@up.ac.th>
2020-09-16 14:06:08 +0700

    [added] function file
    description : function vs arrow function

---
+++ js/function.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+  return a + b;
+}
+function mul(a,b) {
+    return a*b
+    
+}
+const  add = (a,b)=> {
+    return a+b
+}
+const add2 = (a,b) = b+a
+
+console.log(add(2, 3));
