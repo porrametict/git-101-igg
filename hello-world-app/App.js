8aa8515373c6d8e9f5ace807cfcc0994d3d47fd4
Porramet Taeng-orn <37680321+porrametict@users.noreply.github.com>
2020-09-17 14:45:51 +0700

    Update App.js

--- hello-world-app/App.js
+++ hello-world-app/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Image, TextInput, Button } from "react-native";
+import MyButton from "./components/MyButton"
 
 export default function App() {
   const [input, setInput] = useState("");
@@ -22,9 +23,9 @@ export default function App() {
       <Text style={{ fontSize: 18, marginTop: 20, color: color }}>{input}</Text>
 
       <View style={styles.buttonLayout}>
-        <Button title="Red" color="red" onPress={() => {setColor('red')}}></Button>
-        <Button title="Blue" color="blue" onPress={() => {setColor('blue')}}></Button>
-        <Button title="Green" color="green" onPress={() => {setColor('green')}}></Button>
+        <MyButton title="Red" color="red" onChangeColor={(color)=>{setColor(color)}}/>
+        <MyButton title="Blue" color="blue" onChangeColor={(color)=>{setColor(color)}}/>
+        <MyButton title="Green" color="green" onChangeColor={(color)=>{setColor(color)}}/>
       </View>
     </View>
   );
