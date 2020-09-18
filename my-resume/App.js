4f3edbddb44015dde7c0a3688817c1ffe253e803
porrametict <60021414@up.ac.th>
2020-09-18 11:15:10 +0700

    checkout 1

--- my-resume/App.js
+++ my-resume/App.js
@@ -1,24 +1,12 @@
 import * as React from 'react';
-import { View, Text ,Button} from 'react-native';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen(props) {
+import ResumeFromScreen from './screens/ResumeForm'
+function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button title="go to home page" onPress={()=>{
-        props.navigation.push("Detail")
-      }}></Button>
-    </View>
-  );
-}
-
-
-function DetailScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Detail Screen</Text>
     </View>
   );
 }
@@ -29,8 +17,11 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" options={{title :"Home Page"}} component={HomeScreen} />
-        <Stack.Screen name="Detail" options={{title :"Home Page"}} component={DetailScreen} />
+        <Stack.Screen 
+        name="ResumeForm" 
+        options={{title : 'Resume Form'}} 
+        component={ResumeFromScreen}
+         />
       </Stack.Navigator>
     </NavigationContainer>
   );
